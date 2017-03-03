16f1aaf27fd9b0b73a00021d2540aa3e985938e2
Lashkevich Ryan <lashkevichrain@gmail.com>
2017-03-03 15:11:00 +0700

    Removed redundant functionality

--- Edison.TickTackToe.Web/Controllers/HomeController.cs
+++ Edison.TickTackToe.Web/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using Edison.TickTackToe.Web.Resources;
 
 namespace Edison.TickTackToe.Web.Controllers
 {
@@ -15,15 +12,7 @@ namespace Edison.TickTackToe.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = Default.TextApplicationDescriptionPage;
             return View();
         }
     }
